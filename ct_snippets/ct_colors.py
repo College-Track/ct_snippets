0deb02cf515a89fd0528723bade1a64e9d51bedc
Baker Renneckar <bakerrenneckar@gmail.com>
2020-04-23 21:02:29 -0700

    movign

---
+++ ct_snippets/ct_colors.py
@@ -0,0 +1,20 @@
+# College Track Colors
+ct_light_blue = '#00a9e0'
+ct_orange = '#ffb81c'
+ct_purple = '#9063cd'
+ct_grey = '#97999b'
+ct_green = '#78be20'
+ct_dark_blue = '#007398'
+ct_red = '#da291c'
+ct_magenta = '#d0006f'
+
+ct_colors = [
+    ct_light_blue,
+    ct_orange,
+    ct_green,
+    ct_red,
+    ct_purple,
+    ct_grey,
+    ct_magenta,
+    ct_dark_blue,
+]
