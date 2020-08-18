c55469ffb0212993bfd72655181d4eaf000a2b70
Baker Renneckar <bakerrenneckar@gmail.com>
2020-08-17 20:53:52 -0700

    modifying setup

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup, find_packages
 setup(
     name="ct_snippets",
     version="1.0",
-    packages=find_packages(),
+    packages=find_packages(include=["ct_snippets", "ct_snippets.*"]),
 )
