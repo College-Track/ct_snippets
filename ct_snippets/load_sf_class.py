b3ba50cfa5f2a68f13d883c1c30a6f5bae1b7704
Baker Renneckar <bakerrenneckar@gmail.com>
2020-08-19 16:51:38 -0700

    adjusting variable import

--- ct_snippets/load_sf_class.py
+++ ct_snippets/load_sf_class.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 from collections import OrderedDict
-import ct_snippets.variables
+import ct_snippets.variables as variables
 
 
 class SalesforceData:
